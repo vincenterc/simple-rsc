03d36754c7c55073c5c1eb8bd9e846b7bb5b237b
vincenter <vincenterr@yahoo.com.tw>
2024-09-19 15:59:11 +0800

    Implement server side rendering

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,38 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
+import { build as esbuild } from 'esbuild';
+import { fileURLToPath } from 'url';
+import Path, { dirname } from 'path';
+import { renderToString } from 'react-dom/server';
+import { createElement } from 'react';
 
 const app = new Hono();
 
-app.get('/', (c) => c.text('Hello, world!'));
+app.get('/', async (c) => {
+  const { App } = await import('../build/app.js');
+  const html = renderToString(createElement(App));
+  return c.html(html);
+});
 
-serve(app, (info) => console.log(`http://localhost:${info.port}`));
+async function build() {
+  await esbuild({
+    bundle: true,
+    format: 'esm',
+    logLevel: 'error',
+    entryPoints: [resolveSrcPath('app.tsx')],
+    outdir: buildDir,
+    packages: 'external',
+  });
+}
+
+serve(app, async (info) => {
+  await build();
+  console.log(`http://localhost:${info.port}`);
+});
+
+const srcDir = fileURLToPath(dirname(import.meta.url));
+const buildDir = Path.join(srcDir, '../build');
+
+function resolveSrcPath(path = '') {
+  return Path.join(srcDir, path);
+}
